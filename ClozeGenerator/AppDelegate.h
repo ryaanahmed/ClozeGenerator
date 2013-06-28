b79fe0c6a3f339b8fbd015e534ee1032676404e1
Ryaan Ahmed <mail@ryaanahmed.com>
2013-06-28 12:06:07 +0100

    Initial Commit

---
+++ ClozeGenerator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClozeGenerator
+//
+//  Created by Ryaan Ahmed on 6/28/13.
+//  Copyright (c) 2013 Ryaan Ahmed. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ClozeGenerator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ClozeGenerator
+//
+//  Created by Ryaan Ahmed on 6/28/13.
+//  Copyright (c) 2013 Ryaan Ahmed. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
